2a559a78f75e99a417d2b0cf7b9772a20dadaafb
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-06-06 06:31:00 -0700

    added view supply and source modals

--- src/components/products/Supply.js
+++ src/components/products/Supply.js
@@ -9,13 +9,36 @@ import { getSources } from '../../actions/products';
 // ==========
 
 class Supply extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      modal: false,
+      modalClasses: 'modal'
+    };
+    this.toggle = this.toggle.bind(this);
+  };
+
+  toggle () {
+    if (!this.state.modal) {
+      this.setState({
+        modal: true,
+        modalClasses: this.state.modalClasses + ' is-active'
+      });
+    } else {
+      this.setState({
+        modal: false,
+        modalClasses: 'modal'
+      });
+    }
+  };
+
   componentDidMount () {
     this.props.getSources();
   };
-  
+
   render () {
     return (
-      <tr>
+      <tr onClick={this.toggle}>
         <td>{this.props.name}</td>
         <td>
           {
@@ -33,6 +56,41 @@ class Supply extends React.Component {
             ) : null
           }
         </td>
+
+        <div className={this.state.modalClasses}>
+          <div className="modal-background" onClick={this.toggle}></div>
+          <div className="modal-content">
+            <div className="modal-container">
+              <div>
+                <h1 className="title is-3">{this.props.name}</h1>
+                <small>
+                  {
+                    this.props.kinds.find(kind => kind.id === this.props.kind_id) ?
+                    (
+                      this.props.kinds.find(kind => kind.id === this.props.kind_id).name
+                    ) : null
+                  }
+                </small>
+              </div>
+              <div>
+                {
+                  this.props.sources.find(source => source.id === this.props.source_id) ?
+                  (
+                    <span>
+                      <em>from</em>
+                      {this.props.sources.find(source => source.id === this.props.source_id).name}
+                    </span>
+                  ) : null
+                }
+              </div>
+            </div>
+            <div className="supply-control">
+              <div><a href=""><span className="lnr-pencil"></span></a></div>
+              <div><a href=""><span className="lnr-trash2"></span></a></div>
+            </div>
+          </div>
+          <button className="modal-close is-large" aria-label="close" onClick={this.toggle}></button>
+        </div>
       </tr>
     );
   };
