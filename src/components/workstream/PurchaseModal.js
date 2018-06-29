9077a24ecbf196610b47943dd7ab58193d02bac6
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-06-29 02:49:07 -0700

    blocked user interaction to workstream purchase products if not in the right status

--- src/components/workstream/PurchaseModal.js
+++ src/components/workstream/PurchaseModal.js
@@ -246,32 +246,54 @@ class PurchaseModal extends React.Component {
           </div>
 
           <div className={this.state.productsClasses}>
-            <ul>
-              {
-                this.props.purchase.bundles.map(bundle => {
-                  return <PurchaseBundle key={bundle.id} bundle={bundle} purchase={this.props.purchase} toggle={this.toggle} />;
-                })
+            <div
+              className={
+                (() => {
+                  if (this.props.purchase.statuses.find(status => status.status_id === 1).completed === false) {
+                    return 'disable';
+                  } else if (this.props.purchase.statuses.find(status => status.status_id === 1).completed === true && this.props.purchase.statuses.find(status => status.status_id === 2).completed === false) {
+                    return 'disable';
+                  } else if (this.props.purchase.statuses.find(status => status.status_id === 2).completed === true && this.props.purchase.statuses.find(status => status.status_id === 3).completed === false) {
+                    return null;
+                  } else if (this.props.purchase.statuses.find(status => status.status_id === 3).completed === true && this.props.purchase.statuses.find(status => status.status_id === 4).completed === false) {
+                    return 'disable';
+                  } else if (this.props.purchase.statuses.find(status => status.status_id === 4).completed === true) {
+                    return 'disable';
+                  }
+                })()
               }
-              {
-                this.props.purchase.items.map(item => {
-                  return <PurchaseItem key={item.id} item={item} purchase={this.props.purchase} toggle={this.toggle} />;
-                })
-              }
-            </ul>
-            <div className="has-text-centered" style={{padding:'1rem'}}>
-              <button
-                className="button is-small is-primary"
-                disabled={
-                  (() => {
-                    if (((this.props.purchase.items.filter(item => item.completed).length + this.props.purchase.bundles.filter(bundle => bundle.completed).length) / (this.props.purchase.items.length + this.props.purchase.bundles.length)) * 100 === 100) {
-                      return false;
-                    } else {
-                      return true;
-                    }
-                  })()
+            >
+              <ul>
+                {
+                  this.props.purchase.bundles.map(bundle => {
+                    return <PurchaseBundle key={bundle.id} bundle={bundle} purchase={this.props.purchase} toggle={this.toggle} />;
+                  })
                 }
-                onClick={() => this.props.changeStatus(3, true)}
-              >Complete Products</button>
+                {
+                  this.props.purchase.items.map(item => {
+                    return <PurchaseItem key={item.id} item={item} purchase={this.props.purchase} toggle={this.toggle} />;
+                  })
+                }
+              </ul>
+              <div className="has-text-centered" style={{padding:'1rem'}}>
+                <button
+                  className="button is-small is-primary"
+                  disabled={
+                    (() => {
+                      if (this.props.purchase.statuses.find(status => status.status_id === 3).completed === true) {
+                        return true;
+                      } else {
+                        if (((this.props.purchase.items.filter(item => item.completed).length + this.props.purchase.bundles.filter(bundle => bundle.completed).length) / (this.props.purchase.items.length + this.props.purchase.bundles.length)) * 100 === 100) {
+                          return false;
+                        } else {
+                          return true;
+                        }
+                      }  
+                    })()
+                  }
+                  onClick={() => this.props.changeStatus(3, true)}
+                >Complete Products</button>
+              </div>
             </div>
           </div>
 
