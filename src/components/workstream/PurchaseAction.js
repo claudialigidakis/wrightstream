6bc440856c621da6fdb31c065924c70555cd8879
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-07-10 23:38:40 -0700

    workstream added archive button to purchases past delivery date

--- src/components/workstream/PurchaseAction.js
+++ src/components/workstream/PurchaseAction.js
@@ -5,6 +5,9 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
+// HELPERS
+const moment = require('moment');
+
 // ==========
 
 class PurchaseAction extends React.Component {
@@ -25,6 +28,15 @@ class PurchaseAction extends React.Component {
                 return (
                   <footer className="card-footer" onClick={() => this.changeStatus(1, true)}>
                     Move to Pending
+                  </footer>
+                );
+              }
+            } else if (this.props.purchase.statuses.find(status => status.status_id === 4).completed === true) {
+              const today = new Date();
+              if (moment(this.props.purchase.delivery_date).isBefore(today)) {
+                return (
+                  <footer className="card-footer">
+                    Archive
                   </footer>
                 );
               }
