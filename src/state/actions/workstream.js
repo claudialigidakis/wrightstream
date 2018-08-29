8834da6fb292ba6b99a399c74d15c5422530681e
Claudia Ligidakis <30705905+claudialigidakis@users.noreply.github.com>
2018-08-28 18:33:32 -0700

    changed all wrightstream actions to be split within a models folder

--- src/state/actions/workstream.js
+++ src/state/actions/workstream.js
@@ -1,4 +1,4 @@
-import request from '../../helpers/request';
+import WorkStream from '../models/workstream'
 
 export const GET_PURCHASES = 'GET_PURCHASES';
 export const CHANGE_STATUS = 'CHANGE_STATUS';
@@ -11,213 +11,73 @@ export const GET_STAFF = 'GET_STAFF';
 export const ASSIGN_STAFF = 'ASSIGN_STAFF';
 export const ARCHIVE = 'ARCHIVE';
 
-export const getPurchases = () => (
-  dispatch => {
-    request('/auth/token')
-    .then(response => {
-      const shop_id = response.data.shops_id;
-      request(`/purchases/${shop_id}/allPurchases`)
-      .then(response => {
-        return response.data.data.filter(purchase => purchase.archived === false);
-      })
-      .then(response => {
-        dispatch({
-          type: GET_PURCHASES,
-          payload: response
-        });
-      });
-    });
-  }
-);
 
-export const changeStatus = (purchase_id, status_id, completed, staff_id) => (
-  dispatch => {
-    request('/auth/token')
-    .then(response => {
-      const shop_id = response.data.shops_id;
-      request(`/purchases_statuses/${purchase_id}/${status_id}`, 'put', {completed, staff_id})
-      .then(response => {
-        return request(`/purchases/${shop_id}/allPurchases`);
-      })
-      .then(response => {
-        return response.data.data.filter(purchase => purchase.archived === false);
-      })
-      .then(response => {
-        dispatch({
-          type: CHANGE_STATUS,
-          payload: response
-        });
-      });
-    });
-  }
-);
+export const getPurchases = () => {
+  return async (dispatch) => {
+    const payload = await WorkStream.purchases()
+    dispatch({type: GET_PURCHASES, payload});
+  };
+};
 
-export const completeItem = (purchase_id, item_id, staff_id, completed) => (
-  dispatch => {
-    request('/auth/token')
-    .then(response => {
-      const shop_id = response.data.shops_id;
-      request(`/purchases_items/${purchase_id}/${item_id}`, 'put', {staff_id, completed})
-      .then(response => {
-        return request(`/purchases/${shop_id}/allPurchases`);
-      })
-      .then(response => {
-        return response.data.data.filter(purchase => purchase.archived === false);
-      })
-      .then(response => {
-        dispatch({
-          type: COMPLETE_ITEM,
-          payload: response
-        });
-      });
-    });
-  }
-);
+export const changeStatus = (purchase_id, status_id, completed, staff_id) => {
+  return async (dispatch) => {
+    const payload = await WorkStream.changeStatus(purchase_id, status_id, completed, staff_id)
+    dispatch({type: CHANGE_STATUS, payload});
+  };
+};
 
-export const completeBundle = (purchase_id, bundle_id, staff_id, completed) => (
-  dispatch => {
-    request('/auth/token')
-    .then(response => {
-      const shop_id = response.data.shops_id;
-      request(`/purchases_bundles/${purchase_id}/${bundle_id}`, 'put', {staff_id, completed})
-      .then(response => {
-        return request(`/purchases/${shop_id}/allPurchases`);
-      })
-      .then(response => {
-        return response.data.data.filter(purchase => purchase.archived === false);
-      })
-      .then(response => {
-        dispatch({
-          type: COMPLETE_BUNDLE,
-          payload: response
-        });
-      });
-    });
-  }
-);
+export const completeItem = (purchase_id, item_id, staff_id, completed) => {
+  return async (dispatch) => {
+    const payload = await WorkStream.completeItem(purchase_id, item_id, staff_id, completed)
+    dispatch({type: COMPLETE_ITEM, payload});
+  };
+};
 
-export const qualityCheck = (purchase_id, quality_check) => (
-  dispatch => {
-    request('/auth/token')
-    .then(response => {
-      const shop_id = response.data.shops_id;
-      request(`/purchases/${purchase_id}`, 'put', {quality_check})
-      .then(response => {
-        return request(`/purchases/${shop_id}/allPurchases`);
-      })
-      .then(response => {
-        return response.data.data.filter(purchase => purchase.archived === false);
-      })
-      .then(response => {
-        dispatch({
-          type: QUALITY_CHECK,
-          payload: response
-        });
-      });
-    });
-  }
-);
+export const completeBundle = (purchase_id, bundle_id, staff_id, completed) => {
+  return async (dispatch) => {
+    const payload = await WorkStream.completeBundle(purchase_id, bundle_id, staff_id, completed)
+    dispatch({type: COMPLETE_BUNDLE, payload});
+  };
+};
 
-export const schedule = (purchase_id, pick_up, delivery_date, service, tracking) => (
-  dispatch => {
-    request('/auth/token')
-    .then(response => {
-      const shop_id = response.data.shops_id;
-      request(`/purchases/${purchase_id}`, 'put', {pick_up, delivery_date, service, tracking})
-      .then(response => {
-        return request(`/purchases/${shop_id}/allPurchases`);
-      })
-      .then(response => {
-        return response.data.data.filter(purchase => purchase.archived === false);
-      })
-      .then(response => {
-        dispatch({
-          type: SCHEDULE,
-          payload: response
-        });
-      });
-    });
-  }
-);
+export const qualityCheck = (purchase_id, quality_check) => {
+  return async (dispatch) => {
+    const payload = await WorkStream.qualityCheck(purchase_id, quality_check)
+    dispatch({type: QUALITY_CHECK, payload});
+  };
+};
 
-export const addNotes = (purchase_id, notes) => (
-  dispatch => {
-    request('/auth/token')
-    .then(response => {
-      const shop_id = response.data.shops_id;
-      request(`/purchases/${purchase_id}`, 'put', {notes})
-      .then(response => {
-        return request(`/purchases/${shop_id}/allPurchases`);
-      })
-      .then(response => {
-        return response.data.data.filter(purchase => purchase.archived === false);
-      })
-      .then(response => {
-        dispatch({
-          type: ADD_NOTES,
-          payload: response
-        });
-      });
-    });
-  }
-);
+export const schedule = (purchase_id, pick_up, delivery_date, service, tracking) => {
+  return async (dispatch) => {
+    const payload = await WorkStream.schedule(purchase_id, pick_up, delivery_date, service, tracking)
+    dispatch({type: SCHEDULE, payload});
+  };
+};
 
-export const getStaff = () => (
-  dispatch => {
-    request('/auth/token')
-    .then(response => {
-      const shop_id = response.data.shops_id;
-      request(`/shops/${shop_id}/staff`)
-      .then(response => {
-        dispatch({
-          type: GET_STAFF,
-          payload: response.data.data
-        });
-      });
-    });
-  }
-);
+export const addNotes = (purchase_id, notes) => {
+  return async (dispatch) => {
+    const payload = await WorkStream.addNotes(purchase_id, notes)
+    dispatch({type: ADD_NOTES, payload});
+  };
+};
 
-export const assignStaff = (purchase_id, staff_id) => (
-  dispatch => {
-    request('/auth/token')
-    .then(response => {
-      const shop_id = response.data.shops_id;
-      request(`/purchases/${purchase_id}`, 'put', {staff_id})
-      .then(response => {
-        return request(`/purchases/${shop_id}/allPurchases`);
-      })
-      .then(response => {
-        return response.data.data.filter(purchase => purchase.archived === false);
-      })
-      .then(response => {
-        dispatch({
-          type: ASSIGN_STAFF,
-          payload: response
-        });
-      });
-    });
-  }
-);
+export const getStaff = () => {
+  return async (dispatch) => {
+    const payload = await WorkStream.getStaff()
+    dispatch({type: GET_STAFF, payload});
+  };
+};
 
-export const archive = (purchase_id, archived) => (
-  dispatch => {
-    request('/auth/token')
-    .then(response => {
-      const shop_id = response.data.shops_id;
-      request(`/purchases/${purchase_id}`, 'put', {archived})
-      .then(response => {
-        return request(`/purchases/${shop_id}/allPurchases`);
-      })
-      .then(response => {
-        return response.data.data.filter(purchase => purchase.archived === false);
-      })
-      .then(response => {
-        dispatch({
-          type: ARCHIVE,
-          payload: response
-        });
-      });
-    });
-  }
-);
+export const assignStaff = (purchase_id, staff_id) => {
+  return async (dispatch) => {
+    const payload = await WorkStream.assignStaff(purchase_id, staff_id)
+    dispatch({type: ASSIGN_STAFF, payload});
+  };
+};
+
+export const archive = (purchase_id, archived) => {
+  return async (dispatch) => {
+    const payload = await WorkStream.archive(purchase_id, archived)
+    dispatch({type: ARCHIVE, payload});
+  };
+};
--- src/state/models/workstream.js
+++ src/state/models/workstream.js
@@ -1,7 +1,66 @@
-import axios from 'axios'
-const BASE_URL = 'https://galvanize-snacks-api.herokuapp.com/api'
+import request from '../../helpers/request';
 
-class Reviews {
+class WorkStream {
 
+  static _authenticatedRequest = async () => {
+    const authToken = await request('/auth/token')
+    return authToken.data.shops_id
+  }
 
-export default Reviews
+  static purchases = async () => {
+    const shop_id = await WorkStream._authenticatedRequest()
+
+    const response = await request(`/purchases/${shop_id}/allPurchases`)
+    return response.data.data.filter(purchase => purchase.archived === false)
+  }
+
+  static changeStatus = async (purchase_id, status_id, completed, staff_id) => {
+    await request(`/purchases_statuses/${purchase_id}/${status_id}`, 'put', {completed, staff_id})
+    return WorkStream.purchases()
+  }
+
+  static completeItem = async (purchase_id, item_id, staff_id, completed) => {
+    await request(`/purchases_items/${purchase_id}/${item_id}`, 'put', {staff_id, completed})
+    return WorkStream.purchases()
+  }
+
+  static completeBundle = async (purchase_id, bundle_id, staff_id, completed) => {
+    await request(`/purchases_bundles/${purchase_id}/${bundle_id}`, 'put', {staff_id, completed})
+    return WorkStream.purchases()
+  }
+
+  static qualityCheck = async (purchase_id, quality_check) => {
+    await request(`/purchases/${purchase_id}`, 'put', {quality_check})
+    return WorkStream.purchases()
+  }
+
+  static schedule = async (purchase_id, pick_up, delivery_date, service, tracking) => {
+    await request(`/purchases/${purchase_id}`, 'put', {pick_up, delivery_date, service, tracking})
+    return WorkStream.purchases()
+  }
+
+  static addNotes = async (purchase_id, notes) => {
+    await request(`/purchases/${purchase_id}`, 'put', {notes})
+    return WorkStream.purchases()
+  }
+
+  static getStaff = async () => {
+    const shop_id = await WorkStream._authenticatedRequest()
+
+    const staff = await request(`/shops/${shop_id}/staff`)
+    return staff.data.data
+  }
+
+  static assignStaff = async (purchase_id, staff_id) => {
+    await request(`/purchases/${purchase_id}`, 'put', {staff_id})
+    return WorkStream.purchases()
+  }
+
+  static archive = async (purchase_id, archived) => {
+    await request(`/purchases/${purchase_id}`, 'put', {archived})
+    return WorkStream.purchases()
+  }
+
+}
+
+export default WorkStream
