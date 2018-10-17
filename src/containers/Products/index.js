b5b36d187274a287c768640aaf10ce2c467d9105
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-10-16 23:57:05 -0400

    cleaned up code

--- src/containers/Products/index.js
+++ src/containers/Products/index.js
@@ -28,7 +28,6 @@ import Kind from './containers/Supplies/containers/Kind';
 import Sources from './containers/Sources';
 import Type from './containers/Sources/containers/Type';
 
-
 // ==========
 
 class Products extends React.Component {
